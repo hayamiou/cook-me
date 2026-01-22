efe0ee3c3f1ad850813c84c616643a5ff9e89619
Bigmacnib <etienne.bernhard15@gmail.com>
2026-01-22 10:28:46 +0100

    installation generale

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(process.env['MONGODB_URI']!),
-  ],
+  imports: [MongooseModule.forRoot('mongodb://mongo:27017/CMDB')],
   controllers: [AppController],
   providers: [AppService],
 })
