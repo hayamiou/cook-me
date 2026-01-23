4bbaeffea0ef02801ffbd5cee70f16ffd2508641
Kahina <bndkahi73@gmail.com>
2026-01-23 16:48:06 +0100

    feat: creation de module ingredients

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { IngredientsModule } from './ingredients/ingredients.module'
 import { RecipesModule } from './recipes/recipes.module'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo:27017/CMDB'), RecipesModule],
+  imports: [MongooseModule.forRoot('mongodb://mongo:27017/CMDB'), RecipesModule, IngredientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
