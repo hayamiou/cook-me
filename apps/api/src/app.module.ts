92add9dc6f69b8ab1a45d99c221ca2509db138f2
salim skander <salim.skander@ynov.com>
2026-03-02 15:55:03 +0100

    feat: rate limit 10/min par user

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
 import { MongooseModule } from '@nestjs/mongoose'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { IngredientsModule } from './ingredients/ingredients.module'
 import { RecipesModule } from './recipes/recipes.module'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo:27017/CMDB'), RecipesModule, IngredientsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo:27017/CMDB'),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60_000,
+        limit: 10,
+      },
+    ]),
+    RecipesModule,
+    IngredientsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
