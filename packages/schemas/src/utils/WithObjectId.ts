a53043f4611dad36d22afd51454e5a946f2cc491
sacha <sacha.alt@proton.me>
2026-03-03 15:37:50 +0100

    fix: issues with types

--- packages/schemas/src/utils/WithObjectId.ts
+++ packages/schemas/src/utils/WithObjectId.ts
@@ -1,14 +1,11 @@
-import { Types } from 'mongoose'
+import type { Types } from 'mongoose'
 import z from 'zod'
 
 export type WithObjectId<T> = {
   [K in keyof T]: K extends '_id' ? Types.ObjectId : T[K]
 }
 
-export const objectIdSchema = z.union([
-  z
-    .string()
-    .trim()
-    .regex(/^[a-f\d]{24}$/i, 'Must be a valid MongoDB ObjectId'),
-  z.instanceof(Types.ObjectId),
-])
+export const objectIdSchema = z
+  .string()
+  .trim()
+  .regex(/^[a-f\d]{24}$/i, 'Must be a valid MongoDB ObjectId')
